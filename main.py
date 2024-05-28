515064f6712b49a2818d05790f8fb54f3f2cb04c
Nivain_RodrigoV.69 <169165054+nivainverionsixtynine@users.noreply.github.com>
2024-05-29 01:12:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
+# # from turtle import Turtle, Screen
+# #
+# # tim = Turtle()
+# # tim.shape("turtle")
+# # tim.color("red")
+# # for _ in range(4):
+# #     tim.forward(100)
+# #     tim.left(90)
+# #
+# #
+# # screen = Screen()
+# # screen.exitonclick()
+# import random
+# from turtle import Turtle , Screen
+# import turtle as t
+# tim = t.Turtle()
+#
+#
+# # num_sides= 3
+# # for _ in range(num_sides):
+# #     angle = 360/num_sides
+# #     tim.forward(100)
+# #     tim.left(angle)
+# #
+#
+#
+# colours =  ["red","blue","yellow"]
+#
+# def draw_shape(num_sides):
+#     angle = 360/ num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
+#
+#
+# for shape_side_n in range(3,11):
+#     tim.color(random.choice(colours))
+#     draw_shape(shape_side_n)
+
 import turtle as t
 import random
 
 tim = t.Turtle()
-t.colormode(255)
 
-def random_color():
-    r = random.randint(0,255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    random_color = (r,g,b)
-    return random_color
+########### Challenge 4 - Random Walk ########
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
-directions = [0, 90, 180, 270]
-tim.pensize(15)
-tim.speed("fastest")
 
-for _ in range(200):
-    tim.color(random_color())
-    tim.forward(30)
-    tim.setheading(random.choice(directions))
